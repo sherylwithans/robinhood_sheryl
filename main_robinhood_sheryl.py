407cecf889e93288330d0c570322b1dac965ba42
sz2723 <sz2723@columbia.edu>
2020-12-17 15:26:30 -0500

    remove comments from main

--- main_robinhood_sheryl.py
+++ main_robinhood_sheryl.py
@@ -1,11 +1,6 @@
 from robinhood_sheryl.rs_db import *
 
 if __name__ == "__main__":
-    # initTables()
-    # import os
-    # os.environ[
-    #     "GOOGLE_APPLICATION_CREDENTIALS"] = r"C:\Users\shery\OneDrive\Projects\sheryl\quantwannabe-2ef7d7a0a2f7.json"
-    # os.environ["DB_HOST"] = "35.238.1.80:5432"
     login()  # location of robinhood login pickle file
     # insert_yf_data()
     # print(getLatestData(equitiesTable))
