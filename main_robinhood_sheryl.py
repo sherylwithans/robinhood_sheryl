12935673988c5f2df8a55aedace806ace4c96fd6
sz2723 <sz2723@columbia.edu>
2020-12-28 10:30:58 -0500

    delete main comments

--- main_robinhood_sheryl.py
+++ main_robinhood_sheryl.py
@@ -3,4 +3,4 @@ from robinhood_sheryl.rs_db import *
 if __name__ == "__main__":
     login()  # location of robinhood login pickle file
     # insert_yf_data()
-    # print(getLatestData(equitiesTable))
+    print(getLatestData(equitiesTable))
