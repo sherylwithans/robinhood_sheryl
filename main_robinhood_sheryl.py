6acb9c65224f4a6ef232ca4334194e309ed2c5ce
Sheryl Zhou <sz2723@columbia.edu>
2021-01-07 04:30:43 +0000

    update test main

--- main_robinhood_sheryl.py
+++ main_robinhood_sheryl.py
@@ -3,6 +3,6 @@ from robinhood_sheryl.rs_db import *
 if __name__ == "__main__":
     login()  # location of robinhood login pickle file
     # insert_yf_data()
-    # print(getLatestData(equitiesTable))
-    insert_portfolio_data()
+    print(getLatestData(equitiesTable))
+    # insert_portfolio_data()
     # print(pd.isnull(None))
